ef9ae2c94bfde37b86d3b2fdc125a0367834eb1e
Thanh Phong Le <ltphongssvn@gmail.com>
2025-11-20 00:24:54 -0800

    feat: Set dashboard#index as root route

--- manufacturing_erp/config/routes.rb
+++ manufacturing_erp/config/routes.rb
@@ -1,5 +1,8 @@
+# manufacturing_erp/config/routes.rb
 Rails.application.routes.draw do
+  root "dashboard#index"
   get "dashboard/index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,7 +11,7 @@ Rails.application.routes.draw do
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  # get "service-worker" => "rails/pwa#service-worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
   # root "posts#index"
