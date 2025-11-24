88c3f7257a536eafbbfe563ab690a240df351ee2
Thanh Phong Le <ltphongssvn@gmail.com>
2025-11-24 06:50:14 -0800

    feat: Add equipment resource routes

--- manufacturing_erp/config/routes.rb
+++ manufacturing_erp/config/routes.rb
@@ -2,8 +2,10 @@
 Rails.application.routes.draw do
   root "dashboard#index"
   resources :production_lines
+  resources :equipment
   namespace :api do
     resources :production_lines, only: [:index]
+  resources :equipment
   end
   get "dashboard/index"
 
