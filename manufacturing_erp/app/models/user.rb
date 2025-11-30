c27e87887eb35c8bcf28fc01123d33e55323b302
Thanh Phong Le <ltphongssvn@gmail.com>
2025-11-29 18:11:15 -0800

    feat: Add role methods and validations to User model

--- manufacturing_erp/app/models/user.rb
+++ manufacturing_erp/app/models/user.rb
@@ -1,6 +1,41 @@
+# manufacturing_erp/app/models/user.rb
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  ROLES = %w[admin manager operator viewer].freeze
+  
+  after_initialize :set_default_role, if: :new_record?
+  
+  validates :role, inclusion: { in: ROLES }
+  
+  def admin?
+    role == 'admin'
+  end
+  
+  def manager?
+    role == 'manager'
+  end
+  
+  def operator?
+    role == 'operator'
+  end
+  
+  def viewer?
+    role == 'viewer'
+  end
+  
+  def can_edit?
+    admin? || manager?
+  end
+  
+  def can_delete?
+    admin?
+  end
+  
+  private
+  
+  def set_default_role
+    self.role ||= 'viewer'
+  end
 end
